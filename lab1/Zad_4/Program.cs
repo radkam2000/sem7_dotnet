adabf3771a49ed4d58c2fc2a704b06c27809ad55
radkam2000 <radkam2000@protonmail.com>
2023-10-04 13:54:02 +0200

    Zad4 done
    Gra "Zgadnij liczbę"

---
+++ lab1/Zad_4/Program.cs
@@ -0,0 +1,29 @@
+﻿var rand = new Random();
+var value = rand.Next(1, 101);
+
+var result = 0;
+int guess = -1;
+
+while (guess != value)
+{
+    Console.WriteLine("Podaj liczbę");
+    guess = Convert.ToInt32(Console.ReadLine());
+    if (guess < value)
+    {
+        result++;
+        Console.WriteLine("Podana liczba jest za mała");
+    }
+    else if (guess > value)
+    {
+        result++;
+        Console.WriteLine("Podana liczba jest za duża");
+    }
+    else
+    {
+        result++;
+        Console.WriteLine("Wygrałeś w " + result + " ruchach");
+    }
+
+}
+
+//////////////////
--- lab1/Zad_4/obj/Debug/net6.0/Zad_4.AssemblyInfo.cs
+++ lab1/Zad_4/obj/Debug/net6.0/Zad_4.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("lab1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Zad_4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("lab1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("lab1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Zad_4")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Zad_4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Wygenerowane przez klasę WriteCodeFragment programu MSBuild.
