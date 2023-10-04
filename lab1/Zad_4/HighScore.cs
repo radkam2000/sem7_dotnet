565091ee462929e370994d2f0376a3a51943ab50
radkam2000 <radkam2000@protonmail.com>
2023-10-04 14:11:57 +0200

    Added additional functionality
    Added highscores, saving score table

---
+++ lab1/Zad_4/HighScore.cs
@@ -0,0 +1,8 @@
+namespace Zad_4
+{
+    internal class HighScore
+    {
+        public string Name { get; set; }
+        public int Trials { get; set; }
+    }
+}
--- lab1/Zad_4/Program.cs
+++ lab1/Zad_4/Program.cs
@@ -1,7 +1,9 @@
-﻿var rand = new Random();
+﻿using Zad_4;
+using System.Text.Json;
+var rand = new Random();
 var value = rand.Next(1, 101);
 
-var result = 0;
+var trials = 0;
 int guess = -1;
 
 while (guess != value)
@@ -10,20 +12,32 @@ while (guess != value)
     guess = Convert.ToInt32(Console.ReadLine());
     if (guess < value)
     {
-        result++;
+        trials++;
         Console.WriteLine("Podana liczba jest za mała");
-    }
-    else if (guess > value)
-    {
-        result++;
-        Console.WriteLine("Podana liczba jest za duża");
     }
     else
     {
-        result++;
-        Console.WriteLine("Wygrałeś w " + result + " ruchach");
+        trials++;
+        Console.WriteLine("Podana liczba jest za duża");
     }
+}
+trials++;
+Console.WriteLine("Wygrałeś w " + trials + " ruchach");
+Console.WriteLine("Podaj swoje imie:");
+var name = Console.ReadLine();
+var hs = new HighScore { Name = name, Trials = trials };
+List<HighScore> highScores;
+const string FileName = "highscores.json";
 
+if (File.Exists(FileName))
+    highScores = JsonSerializer.Deserialize<List<HighScore>>(File.ReadAllText(FileName));
+else
+    highScores = new List<HighScore>();
+highScores.Add(hs);
+File.WriteAllText(FileName, JsonSerializer.Serialize(highScores));
+foreach (var item in highScores)
+{
+    Console.WriteLine($"{item.Name} -- {item.Trials} prób");
 }
 
 //////////////////
